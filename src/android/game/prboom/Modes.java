c7b1d344c3d65da6422cd2ef990aaa4bdd5b6dd2
Marcelo Martins <martins@cs.brown.edu>
2012-12-10 19:06:36 -0500

    Added modes declaration

---
+++ src/android/game/prboom/Modes.java
@@ -0,0 +1,14 @@
+package android.game.prboom;
+
+
+/**
+ * @author Marcelo Martins <martins@cs.brown.edu>
+ *
+ * Application-mode labels
+ */
+public class Modes {
+
+		public static final String NORMAL_DOOM = "Doom as it should be";
+		public static final String UGLY_DOOM = "Ugly Doom";
+		public static final String NEED_FIX = "I need my Doom fix!";
+}
